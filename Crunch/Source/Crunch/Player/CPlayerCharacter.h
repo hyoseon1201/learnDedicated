f593e3142391b50b923573d2b0865707c47c2967
Hyoseon <wink2090@naver.com>
2025-07-19 13:59:58 +0900

    feat: add Look,Move,Jump Animation

--- Crunch/Source/Crunch/Player/CPlayerCharacter.h
+++ Crunch/Source/Crunch/Player/CPlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Character/CCharacter.h"
+#include "InputActionValue.h"
 #include "CPlayerCharacter.generated.h"
 
 /**
@@ -31,5 +32,18 @@ private:
 	class UInputAction* JumpInputAction;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* LookInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* MoveInputAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
 	class UInputMappingContext* GameplayInputMappingContext;
+
+	void HandleLookInput(const FInputActionValue& InputActionValue);
+	void HandleMoveInput(const FInputActionValue& InputActionValue);
+
+	FVector GetLookRightDir() const;
+	FVector GetLookFwdDir() const;
+	FVector GetMoveFwdDir() const;
 };
