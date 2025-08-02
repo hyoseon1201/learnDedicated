3b9eed745e2e1ccc9f3182e4317ede5785dbda77
Hyoseon <wink2090@naver.com>
2025-08-03 01:23:08 +0900

    feat: Add AI

---
+++ Crunch/Source/Crunch/AI/CAIController.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "CAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CRUNCH_API ACAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	ACAIController();
+
+	virtual void OnPossess(APawn* NewPawn) override;
+
+private:
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	class UAIPerceptionComponent* AIPerceptionComponent;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Perception")
+	class UAISenseConfig_Sight* SightConfig;
+};
